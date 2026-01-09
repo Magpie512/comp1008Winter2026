df0941e85c237e2a71f34803c10c22994877c410
Ben <ben.blanc@georgebrown.ca>
2026-01-08 19:17:16 -0500

    Jan 8 Class Code

---
+++ src/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        /*
+            Object
+            String
+            double
+            float
+            int
+            short
+            long
+            boolean
+            byte
+            char
+
+         */
+        int age = 100;
+        char c = 'A';
+        double d = 3D;  //literal character d/D, f/F, l/L
+        float f = 10000f;
+        byte b1 = (byte)10;  //literal values: whole numbers => INT, decimal number = DOUBLE
+        byte b2 = (byte)20;
+        short s = (short) ( b1 + b2);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class Main {
+    // main+tab, psvm+tab
+
+    public static void main(String[] args) {
+        //sout+tab
+        System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Hello, " + name);
+
+    }
+}
