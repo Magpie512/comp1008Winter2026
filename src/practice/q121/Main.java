0ed9675dc40b6654120c26d77ec5ed8e7b29aca6
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-02-19 14:47:09 -0500

    sd

---
+++ src/practice/q121/Main.java
@@ -0,0 +1,11 @@
+package practice.q121;
+        //Question 12.1
+        //Write a program that:
+        //Asks the user to enter two integers.
+        //Divides the first number by the second.
+        //Uses a try-catch block to handle division by zero.
+        //If division by zero occurs, print: Cannot divide by zero!
+
+public class Main {
+    
+}
