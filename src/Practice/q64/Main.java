b1804ebea7c10ddba10b8453e8ae29417c9adce8
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-02-19 14:37:24 -0500

    bruh

--- src/Practice/q64/Main.java
+++ src/Practice/q64/Main.java
@@ -8,12 +8,11 @@ package practice.q64;
 
 public class Main {
     static void main(String[] args) {
-        Book[] books = new Book[3];
-        books[0] = new Book("The Great Gatsby", "F. Scott Fitzgerald", 10.99);
-        books[1] = new Book("To Kill a Mockingbird", "Harper Lee", 12.99);
-        books[2] = new Book("1984", "George Orwell", 8.99);
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 10.99);
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", 12.99);
+        Book book3 = new Book("1984", "George Orwell", 8.99);
 
-        Book mostExpensiveBook = books[0];
+//        Book mostExpensiveBook = books[0];
 
         /*for (int i = 0; i < books.length; i++){
             if(books[i].getPrice() > mostExpensiveBook.getPrice()){
@@ -21,11 +20,11 @@ public class Main {
             }
         }*/
 
-        for (Book book : books) {
+        /*for (Book book : books) {
             if (book.getPrice() > mostExpensiveBook.getPrice()) {
                 mostExpensiveBook = book;
             }
-        }
+        }*/
 
         System.out.println("The most expensive book is: " + mostExpensiveBook.getTitle() + " by " + mostExpensiveBook.getAuthor() + " with a price of $" + mostExpensiveBook.getPrice());
     }
