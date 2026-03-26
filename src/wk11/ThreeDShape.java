145b5acf101aad830c26a3f020510696f8faa546
Ben <ben.blanc@georgebrown.ca>
2026-03-26 17:52:37 -0400

    Class Code March 26, 2026 @ 17h50

---
+++ src/wk11/ThreeDShape.java
@@ -0,0 +1,16 @@
+package wk11;
+
+public abstract class ThreeDShape extends Shape {
+
+    private double depth;
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    public abstract double volume();
+}
