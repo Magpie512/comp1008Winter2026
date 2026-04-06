8599584aa89c14a5f7dafa7dcc4ff2413005ee8a
Mars Briggs <Marsbriggs512@gmail.com>
2026-04-06 15:51:33 -0400

    a

--- src/wk11/Shape.java
+++ src/wk11/Shape.java
@@ -1,6 +1,8 @@
+// Gen one
 package wk11;
 
-public abstract class Shape {
+public abstract class Shape { 
+// abstract is a restrictive class that cannot be used to make classes
 
     private String name;
 
@@ -8,17 +10,23 @@ public abstract class Shape {
         return name;
     }
 
+    // Setter for name
+    // this.name is the local variable
     public void setName(String name) {
         this.name = name;
     }
 
+    // Constructor with nothing is a default
     public Shape(){}
 
+    // Constructor for name
     public Shape(String name){
         setName(name);
     }
 
     public abstract double area();
+    // An abstract method is a method that has no body
+    // The body is provided by the subclass
 
     public abstract double perimeter();
 
